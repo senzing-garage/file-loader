dd713ce38652f625ce0f59a02bdf42e8b160e147
Ant <ant@senzing.com>
2025-06-03 11:34:40 -0400

    #118 - Fix thread pool drain if non-terminal error occurs

--- file-loader.py
+++ file-loader.py
@@ -12,6 +12,7 @@ import sys
 import textwrap
 import time
 from datetime import datetime
+
 from senzing import (
     G2BadInputException,
     G2ConfigMgr,
@@ -28,9 +29,9 @@ except ModuleNotFoundError:
     import json
 
 __all__ = []
-__version__ = "1.3.0"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.3.5"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = "2022-11-29"
-__updated__ = "2023-12-19"
+__updated__ = "2025-06-03"
 
 
 # Custom actions for argparse. Enables checking if an arg "was specified" on the CLI to check if CLI args should take
@@ -87,11 +88,7 @@ def startup_info(engine, diag, product, configmgr):
         sys.exit(-1)
 
     # Get details for the currently active ID
-    active_cfg_details = [
-        details
-        for details in config_list["CONFIGS"]
-        if details["CONFIG_ID"] == active_cfg_id
-    ]
+    active_cfg_details = [details for details in config_list["CONFIGS"] if details["CONFIG_ID"] == active_cfg_id]
     config_comments = active_cfg_details[0]["CONFIG_COMMENTS"]
     config_created = active_cfg_details[0]["SYS_CREATE_DT"]
 
@@ -109,9 +106,7 @@ def startup_info(engine, diag, product, configmgr):
     logger.info(f"Instance Config ID:         {active_cfg_id}")
     logger.info(f"Instance Config Comments:   {config_comments}")
     logger.info(f"Instance Config Created:    {config_created}")
-    logger.info(
-        f'Hybrid Database:            {"Yes" if db_info["Hybrid Mode"] else "No"}'
-    )
+    logger.info(f'Hybrid Database:            {"Yes" if db_info["Hybrid Mode"] else "No"}')
     logger.info(
         "Database(s):               "
         f" {db_info_type_name[0][0] + ' - ' + db_info_type_name[0][1] if len(db_info_type_name) == 1 else ''}"
@@ -223,16 +218,12 @@ def long_running_check(futures, time_now, num_workers):
                 )
 
     if num_stuck >= num_workers:
-        logger.warning(
-            f"All {num_workers} threads are stuck processing long running records"
-        )
+        logger.warning(f"All {num_workers} threads are stuck processing long running records")
 
 
 def signal_int(signum, frame):
     """Interrupt to allow running threads to finish"""
-    logger.warning(
-        "Please wait for running tasks to complete, this could take many minutes...\n"
-    )
+    logger.warning("Please wait for running tasks to complete, this could take many minutes...\n")
     global do_shutdown
     do_shutdown = True
 
@@ -329,15 +320,11 @@ def load_and_redo(
                         for record in prime_redo_records(engine, executor._max_workers)
                     }
                 logger.info("")
-                logger.info(
-                    f"{mode_text[mode.__name__]['start_msg']} {executor._max_workers} threads..."
-                )
+                logger.info(f"{mode_text[mode.__name__]['start_msg']} {executor._max_workers} threads...")
                 logger.info("")
 
                 while futures:
-                    done, _ = concurrent.futures.wait(
-                        futures, return_when=concurrent.futures.FIRST_COMPLETED
-                    )
+                    done, _ = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
                     for f in done:
                         try:
                             result = f.result()
@@ -361,9 +348,6 @@ def load_and_redo(
                             error_recs += 1
                             do_shutdown = True
                         else:
-                            if add_future and not do_shutdown:
-                                end_of_recs = add_new_future()
-
                             if result:
                                 out_file.write(result + "\n")
 
@@ -376,6 +360,9 @@ def load_and_redo(
                                     mode_text[mode.__name__]["stats_msg"],
                                 )
                         finally:
+                            if add_future and not do_shutdown:
+                                end_of_recs = add_new_future()
+
                             del futures[f]
 
                     if (do_shutdown or end_of_recs) and len(futures) == 0:
@@ -477,7 +464,8 @@ if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(
         allow_abbrev=False,
         description="Utility to load Senzing JSON records and process redo records",
-        epilog=textwrap.dedent("""\
+        epilog=textwrap.dedent(
+            """\
                  Arguments can be specified with either CLI arguments or environment variables, some arguments have
                  default values.
 
@@ -489,7 +477,8 @@ if __name__ == "__main__":
 
                  For additional help and information: https://github.com/senzing-garage/file-loader/blob/main/README.md
 
-               """),
+               """
+        ),
         formatter_class=argparse.RawTextHelpFormatter,
     )
 
@@ -500,13 +489,15 @@ if __name__ == "__main__":
         default=None,
         metavar="file",
         nargs="?",
-        help=textwrap.dedent("""\
+        help=textwrap.dedent(
+            """\
                Path and name of file to load.
 
                Default: None, must be specified.
                Env Var: SENZING_INPUT_FILE
 
-             """),
+             """
+        ),
     )
     arg_parser.add_argument(
         "-cj",
@@ -516,13 +507,15 @@ if __name__ == "__main__":
         metavar="config",
         nargs="?",
         type=str,
-        help=textwrap.dedent("""\
+        help=textwrap.dedent(
+            """\
                JSON string of the Senzing engine configuration.
 
                Default: None, must be specified.
                Env Var: SENZING_ENGINE_CONFIGURATION_JSON
 
-             """),
+             """
+        ),
     )
     arg_parser.add_argument(
         "-i",
@@ -530,13 +523,15 @@ if __name__ == "__main__":
         action=CustomArgActionStoreTrue,
         default=False,
         nargs=0,
-        help=textwrap.dedent("""\
+        help=textwrap.dedent(
+            """\
                Produce withInfo messages and write to a file
 
                Default: False
                Env Var: SENZING_WITHINFO
 
-             """),
+             """
+        ),
     )
     arg_parser.add_argument(
         "-t",
@@ -544,13 +539,15 @@ if __name__ == "__main__":
         action=CustomArgActionStoreTrue,
         default=False,
         nargs=0,
-        help=textwrap.dedent("""\
+        help=textwrap.dedent(
+            """\
                Output debug trace information.
 
                Default: False
                Env Var: SENZING_DEBUG
 
-             """),
+             """
+        ),
     )
     arg_parser.add_argument(
         "-nt",
@@ -559,23 +556,21 @@ if __name__ == "__main__":
         default=0,
         metavar="num_threads",
         type=int,
-        help=textwrap.dedent("""\
+        help=textwrap.dedent(
+            """\
                Total number of worker threads performing load.
 
                Default: Calculated based on hardware.
                Env Var: SENZING_THREADS_PER_PROCESS
 
-             """),
+             """
+        ),
     )
     cli_args = arg_parser.parse_args()
 
     # If a CLI arg was specified use it, else try the env var, if no env var use the default for the CLI arg
     # Sets the priority to 1) CLI arg, 2) Env Var 3) Default value
-    ingest_file = (
-        cli_args.file
-        if cli_args.__dict__.get("file_specified")
-        else os.getenv("SENZING_INPUT_FILE")
-    )
+    ingest_file = cli_args.file if cli_args.__dict__.get("file_specified") else os.getenv("SENZING_INPUT_FILE")
 
     engine_config = (
         cli_args.configJson
@@ -598,12 +593,8 @@ if __name__ == "__main__":
         else int(os.getenv("SENZING_THREADS_PER_PROCESS", cli_args.numThreads))
     )
 
-    errors_file = (
-        f'{MODULE_NAME}_errors_{str(datetime.now().strftime("%Y%m%d_%H%M%S"))}.log'
-    )
-    withinfo_file = (
-        f'{MODULE_NAME}_withInfo_{str(datetime.now().strftime("%Y%m%d_%H%M%S"))}.jsonl'
-    )
+    errors_file = f'{MODULE_NAME}_errors_{str(datetime.now().strftime("%Y%m%d_%H%M%S"))}.log'
+    withinfo_file = f'{MODULE_NAME}_withInfo_{str(datetime.now().strftime("%Y%m%d_%H%M%S"))}.jsonl'
     # If running in a container use /data/
     if os.getenv("SENZING_DOCKER_LAUNCHED"):
         errors_file = f"/data/{errors_file}"
